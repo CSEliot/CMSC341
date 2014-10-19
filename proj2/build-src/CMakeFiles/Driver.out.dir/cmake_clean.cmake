ecfec4baea84c2f50802f7ad9d035db95e3b218d
Eliot <Eliot@localhost>
2014-10-19 19:15:55 -0400

    works on linux, works for me

---
+++ proj2/build-src/CMakeFiles/Driver.out.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Driver.out.dir/Indexer.cpp.o"
+  "CMakeFiles/Driver.out.dir/Driver.cpp.o"
+  "CMakeFiles/Driver.out.dir/Word.cpp.o"
+  "Driver.out.pdb"
+  "Driver.out"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Driver.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ proj2/build-src/CMakeFiles/driver.out.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/driver.out.dir/Indexer.cpp.o"
+  "CMakeFiles/driver.out.dir/Driver.cpp.o"
+  "CMakeFiles/driver.out.dir/Word.cpp.o"
+  "driver.out.pdb"
+  "driver.out"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/driver.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
