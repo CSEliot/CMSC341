c88d2d1789bac0aadff531f4fc7dbcd91c82f714
ur mom <ur mom@localhost>
2014-10-27 00:51:51 -0400

    proj3 added

---
+++ proj3/build-src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/src.dir/driver.cpp.o"
+  "CMakeFiles/src.dir/main.cpp.o"
+  "CMakeFiles/src.dir/Node.cpp.o"
+  "src.pdb"
+  "src"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
